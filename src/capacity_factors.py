Fix average capacity factor
Now weighted by the yearly energy demand.

src/capacity_factors.py
@@ -19,14 +19,21 @@ def capacity_factors(path_to_raw_load, path_to_result):
     )
     cap_factors = national.apply(capacity_factor, axis="index")
     global_cap_factor = capacity_factor(national.sum(axis="columns"))
+    average_cap_factor = average_capacity_factor(cap_factors, national.sum(axis="index"))
     with open(path_to_result, "w") as result_file:
         print("Global capacity factor: {:.2f}".format(global_cap_factor), file=result_file)
-        print("Average national capacity factor: {:.2f}".format(cap_factors.mean()),
+        print("Average national capacity factor: {:.2f}".format(average_cap_factor),
               file=result_file)
 
 
 def capacity_factor(time_series):
+    """Returns the capacity factor of a load or supply time series."""
     return time_series.mean() / time_series.max()
+
+
+def average_capacity_factor(capacity_factors, demands):
+    """Returns the average capacity factors weighted by yearly demand."""
+    return (capacity_factors * demands).sum() / demands.sum()
 
 
 if __name__ == "__main__":

src/vis/capacity_factors.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from src.process_load import read_load_profiles
+from src.capacity_factors import capacity_factor, average_capacity_factor
 
 
 @click.command()
@@ -26,10 +27,10 @@ def visualise_capacity_factors(path_to_load, path_to_plot):
     bars = ax.bar(cap_factors.index, cap_factors.values)
     bars[-1].set_color(plt.rcParams['axes.prop_cycle'].by_key()['color'][1])
     ax.axhline(
-        cap_factors.mean(),
+        average_capacity_factor(cap_factors, national.sum(axis="index")),
         color=plt.rcParams['axes.prop_cycle'].by_key()['color'][3],
         linestyle="dashed",
-        label="EU average",
+        label="national autarky average",
         linewidth=0.75
     )
     plt.legend()
@@ -39,10 +40,6 @@ def visualise_capacity_factors(path_to_load, path_to_plot):
     for tick in ax.get_xticklabels():
         tick.set_rotation(90)
     fig.savefig(path_to_plot, dpi=300)
-
-
-def capacity_factor(time_series):
-    return time_series.mean() / time_series.max()
 
 
 if __name__ == "__main__":