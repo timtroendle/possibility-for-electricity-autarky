f92c0368fba8103d12e847f816ba7cc66f1498d4
Tim Tröndle <tim.troendle@usys.ethz.ch>
2018-06-20 11:52:48 +0200

    Fix eligible land tests
    They were outdated.
    Also refactored them to make them more comprehensible.

--- tests/test_eligible_land.py
+++ tests/test_eligible_land.py
@@ -1,34 +1,49 @@
 import pytest
 import numpy as np
-from numpy.testing import assert_array_equal
 
-from src.eligible_land import Eligibility, determine_eligibility
-
-
-@pytest.fixture
-def land_cover():
-    return np.array([[11, 110], [110, 210], [14, 14]])
+from src.eligible_land import Eligibility, determine_eligibility, GlobCover, ProtectedArea
 
 
 @pytest.fixture
-def protected_areas():
-    return np.array([[0, 0], [1, 0], [0, 0]])
+def config():
+    return {
+        "parameters": {
+            "max-slope": {
+                "pv": 3,
+                "wind": 20
+            },
+            "max-depth-offshore": -50,
+            "max-building-share": 0.1,
+            "max-urban-green-share": 0.1
+        }
+    }
 
 
-@pytest.fixture
-def slope():
-    return np.array([[2, 2], [2, 1], [3, 18]])
+@pytest.mark.parametrize(
+    "land_cover,protected_areas,slope,bathymetry,building_share,urban_green_share,expected", [
+        (GlobCover.RAINFED_CROPLANDS, ProtectedArea.NOT_PROTECTED, 0, 0, 0, 0, Eligibility.ONSHORE_WIND_FARM),
+        (GlobCover.RAINFED_CROPLANDS, ProtectedArea.NOT_PROTECTED, 21, 0, 0, 0, Eligibility.NOT_ELIGIBLE),
+        (GlobCover.RAINFED_CROPLANDS, ProtectedArea.NOT_PROTECTED, 0, 0, 0.11, 0, Eligibility.NOT_ELIGIBLE),
+        (GlobCover.RAINFED_CROPLANDS, ProtectedArea.NOT_PROTECTED, 0, 0, 0, 0.11, Eligibility.NOT_ELIGIBLE),
+        (GlobCover.RAINFED_CROPLANDS, ProtectedArea.PROTECTED, 0, 0, 0, 0, Eligibility.NOT_ELIGIBLE),
+        (GlobCover.MOSAIC_FOREST, ProtectedArea.NOT_PROTECTED, 0, 0, 0, 0, Eligibility.ONSHORE_WIND_FARM),
+        (GlobCover.MOSAIC_GRASSLAND, ProtectedArea.NOT_PROTECTED, 0, 0, 0, 0, Eligibility.ONSHORE_WIND_OR_PV_FARM),
+        (GlobCover.MOSAIC_GRASSLAND, ProtectedArea.NOT_PROTECTED, 4, 0, 0, 0, Eligibility.ONSHORE_WIND_FARM),
+        (GlobCover.WATER_BODIES, ProtectedArea.NOT_PROTECTED, 0, 0, 0, 0, Eligibility.OFFSHORE_WIND_FARM),
+        (GlobCover.WATER_BODIES, ProtectedArea.NOT_PROTECTED, 0, -51, 0, 0, Eligibility.NOT_ELIGIBLE),
+        (GlobCover.WATER_BODIES, ProtectedArea.PROTECTED, 0, 0, 0, 0, Eligibility.NOT_ELIGIBLE)
 
-
-@pytest.fixture
-def bathymetry():
-    return np.array([[20, 20], [200, -45], [30, 18]])
-
-
-def test_eligibility(land_cover, protected_areas, slope, bathymetry):
-    expected_result = np.array([
-        [Eligibility.ONSHORE_WIND_OR_PV_FARM, Eligibility.ONSHORE_WIND_FARM],
-        [Eligibility.NOT_ELIGIBLE, Eligibility.OFFSHORE_WIND_FARM],
-        [Eligibility.ONSHORE_WIND_OR_PV_FARM, Eligibility.ONSHORE_WIND_FARM]
-    ])
-    assert_array_equal(determine_eligibility(land_cover, protected_areas, slope, bathymetry), expected_result)
+    ]
+)
+def test_eligibility(land_cover, protected_areas, slope, bathymetry, building_share, urban_green_share,
+                     expected, config):
+    result = determine_eligibility(
+        land_cover=np.array([land_cover]),
+        protected_areas=np.array([protected_areas]),
+        slope=np.array([slope]),
+        bathymetry=np.array([bathymetry]),
+        building_share=np.array([building_share]),
+        urban_green_share=np.array([urban_green_share]),
+        config=config
+    )
+    assert result[0] == expected
