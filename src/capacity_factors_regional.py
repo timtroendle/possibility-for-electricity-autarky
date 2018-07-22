d085db3921564219d1ca43806764af04706dbc5a
Tim Tröndle <tim.troendle@usys.ethz.ch>
2018-07-22 20:25:37 +0200

    Improve performance of wind capacity factor allocation
    Even in version 0.4.0, geopandas' overlay is very slow and inefficient. Hence, here is my own implmentation. Leads to very similar results, but is much faster.

--- src/capacity_factors_regional.py
+++ src/capacity_factors_regional.py
@@ -26,31 +26,43 @@ def allocate_capacity_factors(path_to_regions, path_to_wind_capacity_factors, pa
     national_cfs = national_cfs.reindex(regions.set_index("country_code").index)
     national_cfs.index = regions.id
 
-    overlay = gpd.overlay(regions, wind_cfs, how="identity")
+    onshore_cfs, offshore_cfs = wind_capacity_factors(wind_cfs, regions)
+
     pd.DataFrame(
         index=regions.id,
         data={
-            "onshore_capacity_factor": overlay.groupby("id").apply(
-                area_weighted_onshore_capacity_factor
-            ).reindex(regions.id),
-            "offshore_capacity_factor": overlay.groupby("id").apply(
-                area_weighted_offshore_capacity_factor
-            ).reindex(regions.id),
+            "onshore_capacity_factor": onshore_cfs,
+            "offshore_capacity_factor": offshore_cfs,
             "pv_capacity_factor": national_cfs["pv_capacity_factor"]
         }
     ).to_csv(path_to_output, index=True, header=True)
 
 
-def area_weighted_onshore_capacity_factor(region):
-    area = area_in_squaremeters(region)
-    normed_area = area / area.sum()
-    return (region["onshore_capacity_factor"] * normed_area).sum()
+def wind_capacity_factors(wind_cfs, regions):
+    wind_cfs["wind_geometry"] = wind_cfs.geometry
+    sjoin = gpd.sjoin(
+        regions,
+        wind_cfs,
+        how="left",
+        op="intersects"
+    )
+    sjoin["intersect_area"] = area_in_squaremeters(
+        gpd.GeoSeries(
+            sjoin.apply(lambda x: x.geometry.intersection(x.wind_geometry), axis=1),
+            crs=regions.crs
+        )
+    )
+    validate_intersection_area(sjoin, regions)
+    normed_area = sjoin.groupby("id")["intersect_area"].transform(lambda x: x / x.sum())
+    onshore_cfs = (normed_area * sjoin["onshore_capacity_factor"]).groupby(sjoin["id"]).sum().reindex(regions.id)
+    offshore_cfs = (normed_area * sjoin["offshore_capacity_factor"]).groupby(sjoin["id"]).sum().reindex(regions.id)
+    return onshore_cfs, offshore_cfs
 
 
-def area_weighted_offshore_capacity_factor(region):
-    area = area_in_squaremeters(region)
-    normed_area = area / area.sum()
-    return (region["offshore_capacity_factor"] * normed_area).sum()
+def validate_intersection_area(sjoin, regions):
+    actual_region_size = area_in_squaremeters(regions).sum()
+    rel_mismatch = (abs(sjoin["intersect_area"].sum() - actual_region_size) / actual_region_size)
+    assert rel_mismatch < 0.0, rel_mismatch
 
 
 if __name__ == "__main__":
