timtroendle/possibility-for-electricity-autarky

commit fe0259da1a9dd2ae8569d74263884ac7377d6e0b

Set normed potentials in units without demand to nan
This became necessary as apparently GDAL 2.3 stopped supporting infinitive values. In any case, an infinite potential does not make sense. Instead, the potential is now nan if these cases.

src/potentials_normed.py
@@ -23,6 +23,7 @@ def normed_potential(paths_to_unit_attributes, path_to_output):
 def determine_normed_potentials(demand_twh_per_year, potentials):
     max_yield_twh_per_year = potentials.sum(axis=1)
     normed_potential = max_yield_twh_per_year / demand_twh_per_year
+    normed_potential[normed_potential == pd.np.inf] = pd.np.nan # handle regions with no demand
     normed_potential.name = "normed_potential"
     normed_potential.index.name = "id"
     return normed_potential

commit 89dc613bdcb44506d93665c498d79ba24a6f3f03

Fix infinite normed potentials by switching to GPKG
This was a regression introduced in fe0259da1a9dd2ae8569d74263884ac7377d6e0b and by updating geo libraries in e96545fa542af33be58cc9e78ed6588a8e00968d. Strictly speaking, GeoJSON does not support inf values. GDAL or Fiona was changed in  a way to adhere to this strict standard, leading me to falsly set inf values to nan. That however caused those units to be removed from the map.

Now inf potentials remain inf by storing them in a GPKG instead of GeoJSON file.

See for example https://github.com/Toblerity/Fiona/issues/684.

This reverts commit fe0259da1a9dd2ae8569d74263884ac7377d6e0b.

rules/analysis.smk
@@ -41,7 +41,7 @@ rule normed_potential_boxplots:
     message: "Plot ranges of relative potential for scenario {wildcards.scenario}."
     input:
         "src/vis/potentials_normed_boxplot.py",
-        "build/municipal/{scenario}/merged-results.geojson"
+        "build/municipal/{scenario}/merged-results.gpkg"
     output:
         "build/{scenario}/normed-potentials-boxplots.{suffix}"
     conda: "../envs/default.yaml"
@@ -53,10 +53,10 @@ rule potentials_sufficiency_map:
     message: "Plot potential sufficiency maps for scenario {wildcards.scenario}."
     input:
         "src/vis/potentials_sufficiency_map.py",
-        "build/continental/{scenario}/merged-results.geojson",
-        "build/national/{scenario}/merged-results.geojson",
-        "build/regional/{scenario}/merged-results.geojson",
-        "build/municipal/{scenario}/merged-results.geojson"
+        "build/continental/{scenario}/merged-results.gpkg",
+        "build/national/{scenario}/merged-results.gpkg",
+        "build/regional/{scenario}/merged-results.gpkg",
+        "build/municipal/{scenario}/merged-results.gpkg"
     output:
         "build/{scenario}/sufficient-potentials-map.{suffix}"
     conda: "../envs/default.yaml"

rules/data-preprocessing.smk
@@ -123,7 +123,7 @@ rule administrative_borders_lau:
         src = "src/lau.py",
         zip = rules.raw_lau_units_zipped.output
     output:
-        "build/administrative-borders-lau.gpkg"
+        "build/administrative-borders-lau.geojson"
     shadow: "full"
     conda: "../envs/default.yaml"
     shell:

rules/potential.smk
@@ -381,7 +381,7 @@ rule scenario_results:
         constrained_potentials = rules.potentials.output,
         normed_potentials = rules.normed_potentials.output
     output:
-        "build/{layer}/{scenario}/merged-results.geojson"
+        "build/{layer}/{scenario}/merged-results.gpkg"
     run:
         import pandas as pd
         import geopandas as gpd
@@ -393,4 +393,4 @@ rule scenario_results:
             ),
             left_on="id",
             right_index=True
-        ).to_file(output[0], driver="GeoJSON")
+        ).to_file(output[0], driver="GPKG")

src/potentials_normed.py
@@ -23,7 +23,6 @@ def normed_potential(paths_to_unit_attributes, path_to_output):
 def determine_normed_potentials(demand_twh_per_year, potentials):
     max_yield_twh_per_year = potentials.sum(axis=1)
     normed_potential = max_yield_twh_per_year / demand_twh_per_year
-    normed_potential[normed_potential == pd.np.inf] = pd.np.nan # handle regions with no demand
     normed_potential.name = "normed_potential"
     normed_potential.index.name = "id"
     return normed_potential